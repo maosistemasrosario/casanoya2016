c01c5f42864dbb2dd3640ecc9bd9c83804f04e2c
Anibal Schiaves <anibalschiaves@hotmail.com>
2016-09-23 21:25:22 -0300

    ajuste en config.php

--- sistema/system/application/config/config.php
+++ sistema/system/application/config/config.php
@@ -12,7 +12,7 @@
 |
 */
 //$config['base_url']	= "http://www.imagining4u.com/delos";
-$config['base_url']	= "http://www.imagining4u.com/casanoya2016/sistema/";
+$config['base_url']	= "http://localhost/casanoya2016/sistema/";
 //$config['base_url']	= "http://www.cartopel.com.ar/sistema/";
 
 /*
